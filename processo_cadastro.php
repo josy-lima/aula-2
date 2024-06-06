f1c040f0b4de8d4e447265bae6fba33a53544883
maleficent123 <joseildabarbosalima@gmail.com>
2024-06-06 10:13:55 -0300

    Atualizar o script PHP em processo_cadastro.php

--- processo_cadastro.php
+++ processo_cadastro.php
@@ -1,4 +1,32 @@
 <?php
+// Coletando dados do formulario
+$nome = isset($_POST['nome']) ? $_POST['nome'] : 'Anonimo';
+$email = $_POST['email'] ?? 'Nao informado';
+$idade = $_POST['idade'] ?? 'Nao informado';
+
+// Armazenando dados em um array associativo
+$usuario = [
+    'nome' => $nome,
+    'email'=> $email,
+    'idade'=> $idade
+];
+
+// Funcao para saudacao
+function saudacao($nome) {
+    return "Ola $nome, seja bem vindo ao nosso sistema!";
+}
+
+// Exibindo uma mensagem de boas-vindas
+echo saudacao($usuario['nome']) . "<br>";
+echo "Aqui estao os detalhes do seu cadastro:<br>";
+echo "Nome: " . $usuario['nome'] . "<br>";
+echo "Email: " . $usuario['email'] . "<br>";
+echo "Idade: " . $usuario['idade'] . "<br>";
+
+// Adicionando comentarios explicativos sobre o codigo
+// Este script processa as entradas de um formulario e exibe uma mensagem personalizada
+?>
+<?php
 if (["REQUEST_METHOD"] == "POST") {
     $nome = $_POST['nome'];
     $email = $_POST['email'];
